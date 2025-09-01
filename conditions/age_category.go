2882822183676117531c942bb1c5d4593c5ef461
Andrew Nishanov <mazahaker93@gmail.com>
2025-09-01 12:39:07 +0600

    category by age task

---
+++ conditions/age_category.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Age category by age")
+
+	var age int
+	fmt.Print("Please, input age: ")
+	fmt.Scanln(&age)
+
+	if age < 12 {
+		fmt.Println("It's a child")
+	} else if age >= 12 || age < 18 {
+		fmt.Println("It's a teenager")
+	} else if age >= 18 || age < 60 {
+		fmt.Println("It's a adult")
+	} else {
+		fmt.Println("It's a pensioner")
+	}
+}
